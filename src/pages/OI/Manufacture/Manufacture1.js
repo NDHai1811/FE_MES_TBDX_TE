41c7c0bc9dfb7f298d673cc8c86e25ab9da649c6
tranvanhuu <tranthhienlong@gmail.com>
2023-11-22 11:10:22 +0700

    1

--- src/pages/OI/Manufacture/Manufacture1.js
+++ src/pages/OI/Manufacture/Manufacture1.js
@@ -391,31 +391,31 @@ const Manufacture1 = (props) => {
       setIsOpenQRScanner(false);
     }
   }, [isScan]);
-  var token = 'eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJtZXNzeXN0ZW1AZ21haWwuY29tIiwidXNlcklkIjoiNGQxYzg5NTAtODVkOC0xMWVlLTgzOTItYTUxMzg5MTI2ZGM2Iiwic2NvcGVzIjpbIlRFTkFOVF9BRE1JTiJdLCJzZXNzaW9uSWQiOiI0Mjc4OTdlMi0xYzIyLTRjMmItOTEzMS00ZDRkOTYzMjc0NDUiLCJpc3MiOiJ0aGluZ3Nib2FyZC5pbyIsImlhdCI6MTcwMDU1MDU2MiwiZXhwIjoxNzAwNTU5NTYyLCJlbmFibGVkIjp0cnVlLCJpc1B1YmxpYyI6ZmFsc2UsInRlbmFudElkIjoiMzYwY2MyMjAtODVkOC0xMWVlLTgzOTItYTUxMzg5MTI2ZGM2IiwiY3VzdG9tZXJJZCI6IjEzODE0MDAwLTFkZDItMTFiMi04MDgwLTgwODA4MDgwODA4MCJ9.Z8WohLv48FCT-k3Y7o4hDvHiK5DXETFQSjlWG2sO60qcXG03FJwytp9KVAbE2s7ZkSfJSSbnTo-_DDQFhKj0_A';
-  var entityID = 'e9aba8d0-85da-11ee-8392-a51389126dc6';
-  var webSocket = new WebSocket("ws://113.176.95.167:3030/api/plugins/telemetry?token="+token);
-  webSocket.onopen = function (){
-    var object = {
-      tsSubCmds : [{
-        entityType:'DEVICE',
-        entityID:entityID,
-        scope:'LATEST_TELEMETRY',
-        cmdId:10
-      }],
-      historyCmds:[],
-      attrSubCmds:[],
-    }
-    var data = JSON.stringify(object);
-    webSocket.send(data);
-    console.log(data);
-  }
-  webSocket.onmessage = function(event){
-    var recived_msg = event.data;
-    console.log(recived_msg);
-  }
-  webSocket.onclose = function(event){
-    console.log('Connection is closed');
-  }
+  // var token = 'eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJtZXNzeXN0ZW1AZ21haWwuY29tIiwidXNlcklkIjoiNGQxYzg5NTAtODVkOC0xMWVlLTgzOTItYTUxMzg5MTI2ZGM2Iiwic2NvcGVzIjpbIlRFTkFOVF9BRE1JTiJdLCJzZXNzaW9uSWQiOiI0Mjc4OTdlMi0xYzIyLTRjMmItOTEzMS00ZDRkOTYzMjc0NDUiLCJpc3MiOiJ0aGluZ3Nib2FyZC5pbyIsImlhdCI6MTcwMDU1MDU2MiwiZXhwIjoxNzAwNTU5NTYyLCJlbmFibGVkIjp0cnVlLCJpc1B1YmxpYyI6ZmFsc2UsInRlbmFudElkIjoiMzYwY2MyMjAtODVkOC0xMWVlLTgzOTItYTUxMzg5MTI2ZGM2IiwiY3VzdG9tZXJJZCI6IjEzODE0MDAwLTFkZDItMTFiMi04MDgwLTgwODA4MDgwODA4MCJ9.Z8WohLv48FCT-k3Y7o4hDvHiK5DXETFQSjlWG2sO60qcXG03FJwytp9KVAbE2s7ZkSfJSSbnTo-_DDQFhKj0_A';
+  // var entityID = 'e9aba8d0-85da-11ee-8392-a51389126dc6';
+  // var webSocket = new WebSocket("ws://113.176.95.167:3030/api/ws/plugins/telemetry?token="+token);
+  // webSocket.onopen = function (){
+  //   var object = {
+  //     tsSubCmds : [{
+  //       entityType:'DEVICE',
+  //       entityID:entityID,
+  //       scope:'LATEST_TELEMETRY',
+  //       cmdId:10
+  //     }],
+  //     historyCmds:[],
+  //     attrSubCmds:[],
+  //   }
+  //   var data = JSON.stringify(object);
+  //   webSocket.send(data);
+  //   console.log(data);
+  // }
+  // webSocket.onmessage = function(event){
+  //   var recived_msg = event.data;
+  //   console.log(recived_msg);
+  // }
+  // webSocket.onclose = function(event){
+  //   console.log('Connection is closed');
+  // }
 
   return (
     <React.Fragment>
