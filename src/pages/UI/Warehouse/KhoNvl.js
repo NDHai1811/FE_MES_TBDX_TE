6106f96eb2b241b69df1111cdf13cd3e65a96a5b
AnPham <phamthithuyan26091996@gmail.com>
2024-02-24 08:51:12 +0700

    1

--- src/pages/UI/Warehouse/KhoNvl.js
+++ src/pages/UI/Warehouse/KhoNvl.js
@@ -19,7 +19,7 @@ import {
   exportBMCardWarehouse,
   exportSummaryWarehouse,
 } from "../../../api";
-import { exportWarehouse } from "../../../api/ui/export";
+import { exportWarehouse, exportWarehouseMLTLogs } from "../../../api/ui/export";
 import { baseURL } from "../../../config";
 import dayjs from "dayjs";
 import { COMMON_DATE_FORMAT } from "../../../commons/constants";
@@ -148,8 +148,8 @@ const table1 = [
   },
   {
     title: "Sl đầu (kg)",
-    dataIndex: "sl_dau",
-    key: "sl_dau",
+    dataIndex: "so_kg_dau",
+    key: "so_kg_dau",
     align: "center",
     render: (value) => value || "-",
   },
@@ -162,8 +162,8 @@ const table1 = [
   },
   {
     title: "Sl cuối (kg)",
-    dataIndex: "sl_cuoi",
-    key: "sl_cuoi",
+    dataIndex: "so_kg_cuoi",
+    key: "so_kg_cuoi",
     align: "center",
     render: (value) => value || "-",
   },
@@ -250,10 +250,14 @@ const KhoNvl = (props) => {
   document.title = "UI - Quản lý kho NVL";
   const [dataTable, setDataTable] = useState([]);
   const [params, setParams] = useState({ date: [dayjs(), dayjs()] });
+  const [form] = Form.useForm();
+  const [page, setPage] = useState(1);
+  const [pageSize, setPageSize] = useState(10);
+  const [totalPage, setTotalPage] = useState(1);
   const [exportLoading, setExportLoading] = useState(false);
   const exportFile = async () => {
     setExportLoading(true);
-    const res = await exportWarehouse(params);
+    const res = await exportWarehouseMLTLogs(params);
     if (res.success) {
       window.location.href = baseURL + res.data;
     }
@@ -261,13 +265,17 @@ const KhoNvl = (props) => {
   };
   async function btn_click() {
     setLoading(false);
-    const res = await getHistoryWareHouseMLT(params);
-    setDataTable(res);
+    const res = await getHistoryWareHouseMLT(form.getFieldsValue(true));
+    setDataTable(res.data);
+    setTotalPage(res.totalPage);
     setLoading(false);
   }
   useEffect(() => {
     btn_click();
   }, [])
+  const onFinish = async () =>{
+    
+  }
   const [loading, setLoading] = useState(false);
   const [exportLoading1, setExportLoading1] = useState(false);
   const items = [
@@ -313,11 +321,11 @@ const KhoNvl = (props) => {
       setExportLoading1(false);
     }
   }
-
+  
   return (
     <>
       {contextHolder}
-      <Row style={{ padding: "8px", height: "90vh" }} gutter={[8, 8]}>
+      <Row style={{ padding: "8px", marginRight: 0 }} gutter={[8, 8]}>
         <Col span={4}>
           <div className="slide-bar">
             <Card
@@ -328,6 +336,7 @@ const KhoNvl = (props) => {
                 <div layout="vertical">
                   <Button
                     type="primary"
+                    htmlType="submit"
                     style={{ width: "80%" }}
                     onClick={btn_click}
                   >
@@ -336,8 +345,8 @@ const KhoNvl = (props) => {
                 </div>,
               ]}
             >
-              <Divider>Thời gian truy vấn</Divider>
-              <div className="mb-3">
+              {/* <Divider>Thời gian truy vấn</Divider> */}
+              {/* <div className="mb-3">
                 <Form style={{ margin: "0 15px" }} layout="vertical">
                   <Space direction="vertical" style={{ width: "100%" }}>
                     <DatePicker
@@ -345,9 +354,9 @@ const KhoNvl = (props) => {
                       placeholder="Bắt đầu"
                       style={{ width: "100%" }}
                       onChange={(value) =>
-                        setParams({ ...params, date: [value, params.date[1]] })
+                        setParams({ ...params, start_date: value })
                       }
-                      value={params.date[0]}
+                      value={params.start_date}
                       format={COMMON_DATE_FORMAT}
                     />
                     <DatePicker
@@ -355,71 +364,70 @@ const KhoNvl = (props) => {
                       placeholder="Kết thúc"
                       style={{ width: "100%" }}
                       onChange={(value) =>
-                        setParams({ ...params, date: [params.date[0], value] })
+                        setParams({ ...params, end_date: value })
                       }
-                      value={params.date[1]}
+                      value={params.end_date}
                       format={COMMON_DATE_FORMAT}
                     />
                   </Space>
                 </Form>
-              </div>
+              </div> */}
               <Divider>Điều kiện truy vấn</Divider>
               <div className="mb-3">
-                <Form style={{ margin: "0 15px" }} layout="vertical">
-                  <Form.Item label="Loại giấy" className="mb-3">
-                    <Input placeholder="Nhập loại giấy" />
+                <Form style={{ margin: "0 15px" }} layout="vertical" form={form} onFinish={btn_click}>
+                  <Form.Item label="Loại giấy" className="mb-3" name={"loai_giay"}>
+                    <Input allowClear placeholder="Nhập loại giấy" onChange={(event)=>form.setFieldsValue({...form.getFieldsValue(true), loai_giay: event.target.value})}/>
                   </Form.Item>
-                  <Form.Item label="Khổ giấy" className="mb-3">
-                    <Input placeholder="Nhập khổ giấy" />
+                  <Form.Item label="Khổ giấy" className="mb-3" name={"kho_giay"}>
+                    <Input allowClear placeholder="Nhập khổ giấy"  onChange={(event)=>form.setFieldsValue({...form.getFieldsValue(true), kho_giay: event.target.value})}/>
                   </Form.Item>
-                  <Form.Item label="Định lượng" className="mb-3">
-                    <Input placeholder="Nhập định lượng" />
+                  <Form.Item label="Định lượng" className="mb-3" name={"dinh_luong"}>
+                    <Input allowClear placeholder="Nhập định lượng"  onChange={(event)=>form.setFieldsValue({...form.getFieldsValue(true), dinh_luong: event.target.value})}/>
                   </Form.Item>
-                  <Form.Item label="Mã cuộn NCC" className="mb-3">
-                    <Input placeholder="Nhập mã cuộn NCC" />
+                  <Form.Item label="Mã cuộn NCC" className="mb-3" name={"ma_cuon_ncc"}>
+                    <Input allowClear placeholder="Nhập mã cuộn NCC"  onChange={(event)=>form.setFieldsValue({...form.getFieldsValue(true), ma_cuon_ncc: event.target.value})}/>
                   </Form.Item>
-                  <Form.Item label="Mã vật tư" className="mb-3">
-                    <Input placeholder="Nhập mã vật tư" />
+                  <Form.Item label="Mã vật tư" className="mb-3" name={"ma_vat_tu"}>
+                    <Input allowClear placeholder="Nhập mã vật tư"  onChange={(event)=>form.setFieldsValue({...form.getFieldsValue(true), ma_vat_tu: event.target.value})}/>
                   </Form.Item>
-                  <Form.Item label="Ngày nhập" className="mb-3">
+                  <Form.Item label="Ngày nhập" className="mb-3" name={"tg_nhap"}>
                     <DatePicker
-                      allowClear={false}
+                      allowClear={true}
                       placeholder="Ngày nhập"
                       style={{ width: "100%" }}
                       onChange={(value) =>
-                        setParams({ ...params, tg_nhap: value })
+                        form.setFieldsValue({ ...form.getFieldsValue(true), tg_nhap: value })
                       }
-                      value={params.tg_nhap}
                       format={COMMON_DATE_FORMAT}
                     />
                   </Form.Item>
-                  <Form.Item label="Số phiếu nhập kho" className="mb-3">
-                    <Input placeholder="Nhập số phiếu nhập kho" />
+                  <Form.Item label="Số phiếu nhập kho" className="mb-3" name={"goods_receipt_note_id"}>
+                    <Input allowClear placeholder="Nhập số phiếu nhập kho"  onChange={(event)=>form.setFieldsValue({...form.getFieldsValue(true), goods_receipt_note_id: event.target.value})}/>
                   </Form.Item>
-                  <Form.Item label="Số lượng cuối" className="mb-3">
-                    <Input placeholder="Nhập số lượng cuối" />
+                  <Form.Item label="Số lượng cuối" className="mb-3" name={"so_kg"}>
+                    <Input allowClear placeholder="Nhập số lượng cuối"  onChange={(event)=>form.setFieldsValue({...form.getFieldsValue(true), so_kg: event.target.value})}/>
                   </Form.Item>
-                  <Form.Item label="Ngày xuất" className="mb-3">
+                  <Form.Item label="Ngày xuất" className="mb-3" name={"tg_xuat"}>
                     <DatePicker
-                      allowClear={false}
+                      allowClear={true}
                       placeholder="Ngày xuất"
                       style={{ width: "100%" }}
                       onChange={(value) =>
-                        setParams({ ...params, tg_xuat: value })
+                        form.setFieldsValue({ ...form.getFieldsValue(true), tg_xuat: value })
                       }
-                      value={params.tg_xuat}
                       format={COMMON_DATE_FORMAT}
                     />
                   </Form.Item>
-                  <Form.Item label="Số cuộn" className="mb-3">
-                    <Input placeholder="Nhập số lượng cuối" />
+                  <Form.Item label="Số cuộn" className="mb-3" name={"so_cuon"}>
+                    <Input allowClear placeholder="Nhập cuộn"  onChange={(event)=>form.setFieldsValue({...form.getFieldsValue(true), so_cuon: event.target.value})}/>
                   </Form.Item>
-                  <Form.Item label="Khu vực" className="mb-3">
-                    <Input placeholder="Nhập khu vực" />
+                  <Form.Item label="Khu vực" className="mb-3" name={"khu_vuc"}>
+                    <Input allowClear placeholder="Nhập khu vực"  onChange={(event)=>form.setFieldsValue({...form.getFieldsValue(true), khu_vuc: event.target.value})}/>
                   </Form.Item>
-                  <Form.Item label="Vị trí" className="mb-3">
-                    <Input placeholder="Nhập vị trí" />
+                  <Form.Item label="Vị trí" className="mb-3" name={"locator_id"}>
+                    <Input allowClear placeholder="Nhập vị trí"  onChange={(event)=>form.setFieldsValue({...form.getFieldsValue(true), locator_id: event.target.value})}/>
                   </Form.Item>
+                  <Button hidden htmlType="submit"></Button>
                 </Form>
               </div>
             </Card>
@@ -447,7 +455,17 @@ const KhoNvl = (props) => {
               <Table
                 size="small"
                 bordered
-                pagination={false}
+                pagination={{
+                  current: page,
+                  size: "small",
+                  total: totalPage,
+                  showSizeChanger: true,
+                  onChange: (page, pageSize) => {
+                    setPage(page);
+                    setPageSize(pageSize);
+                    setParams({ ...params, page: page, pageSize: pageSize });
+                  },
+                }}
                 scroll={{
                   x: "130vw",
                   y: "70vh",
