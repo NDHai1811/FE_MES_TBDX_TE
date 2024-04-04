6136cca9c44b97666c119e359ee3152154646269
AnPham <phamthithuyan26091996@gmail.com>
2024-04-04 08:59:06 +0700

    1

--- src/pages/OI/Warehouse/finished-product/export.js
+++ src/pages/OI/Warehouse/finished-product/export.js
@@ -283,6 +283,7 @@ const Export = (props) => {
       setVisible(false);
       form.resetFields();
       loadData();
+      loadDataTable();
     }
   }
   const [isDownloading, setIsDownloading] = useState(false)
