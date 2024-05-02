221ca44310ac965c3b6e88e4da3266cbe5b50351
tranvanhuu <tranthhienlong@gmail.com>
2024-05-02 10:49:49 +0700

    config link

--- src/config.js
+++ src/config.js
@@ -8,7 +8,7 @@ module.exports = {
     APP_ID: "",
   },
   api: {
-    API_URL: "http://127.0.0.1:8000/api",
+    API_URL: "https://tbdxback.ors.vn/api",
   },
-  baseURL: "http://127.0.0.1:8000",
+  baseURL: "https://tbdxback.ors.vn",
 }
